87d9758c8c2d9f9f90bf357a03baf90342340b45
cleiton <djcleitonduarte@hotmail.com>
2021-05-07 10:49:05 -0400

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -25,11 +25,6 @@ const DonuChart = () => {
   }, []);
 
 
-  const mockData = {
-    series: [477138, 499928, 444867, 220426, 473088],
-    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-  }
-
   const options = {
     legend: {
       show: true
@@ -37,8 +32,8 @@ const DonuChart = () => {
   };
   return (
     <Chart
-      options={{ ...options, labels: mockData.labels }}
-      series={mockData.series}
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
       type="donut"
       height="240"
     />
